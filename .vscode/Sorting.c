47893ed94fb38a2d90437797f79e68e576acd039
MVK2803 <mathewvinukariath@gmail.com>
2023-01-15 20:04:55 +0530

    quick sort

--- .vscode/Sorting.c
+++ .vscode/Sorting.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int bc[10]={1,2,3,4,5,6,7,8,9,10};
-int wc[10]={1,2,0,3,-1,7,8,0,1,2};
+int wc[8]={-1,15,5,20,1,17,10,30};
 
 void swap(int A[] ,int a,int b)
 {
@@ -127,11 +127,58 @@ void merge_sort(int a[],int lb,int ub)
         merge_array(a,lb,mid,ub);
     }
 }
+void max_heapify(int a[],int n,int i)
+{
+    int largest=i;
+    int r=i*2+1;
+    int l=i*2;
+    //printf("%d%d%d%d ",i,r,l,n);
+    if(l<=n && (a[largest]<=a[l]))
+    {
+        largest=l;
+    }
+    if(r<=n && (a[largest]<=a[r]))
+    {
+        largest=r;
+    }
+    //printf("%d",largest);
+    
+    if(largest!=i)
+    {
+        swap(a,i,largest);
+        max_heapify(a,n,largest);
+    }
+    for(int i=1;i<=7;++i)
 
+    {
+        //printf("%d ",wc[i]);
+    }
+    printf("\n");
+
+}
+void heap_sort(int A[],int n)
+{
+    for(int i=n/2;i>=1;i--)
+    {   //printf("%d",i);
+        max_heapify(A,n,i);
+    }
+    printf("==========");
+    for(int i=n;i>=1;--i)
+    {   //printf("doinf");
+        swap(A,1,i);
+        //printf("after swap\n");
+        for(int j=1;j<=7;j++)
+    {
+       // printf("%d ",wc[j]);
+    }
+    printf("\n");
+        max_heapify(A,i-1,1);
+    }
+}
 void main()
 {
-    merge_sort(wc,0,9);
-    for(int i=0;i<10;++i)
+    heap_sort(wc,7);
+    for(int i=1;i<=7;++i)
     {
         printf("%d ",wc[i]);
     }
